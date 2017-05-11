d303306f6300b35fef25e4d13c715bb1c7e5a3f0
Yiying Wang <wang.yiying@hotmail.com>
2017-05-11 14:15:57 -0700

    did number 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,11 @@
 # Exercise 1: Loading functions
 
 # Set your directory
-
+setwd("~/Desktop/School/info201/m18-shiny/exercise-1/")
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source("./scripts/BuildScatter.R")
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+BuildScatterPlot(data = iris, x.axis = "Sepal.Length", y.axis = "Sepal.Width", var.color = "Species", title = "Iris Dataset", x.label = "Sepal Length", y.label = "Sepal.Width")
+
--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -1,5 +1,5 @@
 # Function to build a scatterplot
-
+library(ggplot2)
 # Write your function to parameterize the following variables:
 #   - Data to use
 #   - Variable for the x axis
@@ -8,3 +8,10 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+BuildScatterPlot <- function(data, x.axis, y.axis, var.color, title = "Title", x.label = "X Title", y.label = "Y Title") {
+  plot <- ggplot(data = data) +
+    geom_point(mapping = aes(x = data[, x.axis], y = data[, y.axis], color = data[, var.color])) +
+    labs(title = title, x = x.label, y = y.label)
+  return(plot)
+}
